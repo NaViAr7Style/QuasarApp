5f146f28c6e2c2d205a78deae86d73e16db02fec
NaViAr7Style <ar7styleblizz@gmail.com>
2023-11-11 04:45:08 +0200

    Working on project skeleton

---
+++ QuasarApp/src/main/java/bg/deliev/quasarapp/model/entities/OrderEntity.java
@@ -0,0 +1,9 @@
+package bg.deliev.quasarapp.model.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "orders")
+public class OrderEntity extends BaseEntity {
+}
---
+++ QuasarApp/src/main/java/bg/deliev/quasarapp/model/entities/UserEntity.java
@@ -0,0 +1,9 @@
+package bg.deliev.quasarapp.model.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class UserEntity extends BaseEntity {
+}
