8e3e5a44fdfa37d9ab489b318d5e5e129db3a45a
NaViAr7Style <ar7styleblizz@gmail.com>
2025-05-04 01:37:06 +0300

    Optimized imports.

--- src/test/java/bg/deliev/quasarapp/web/controller/UserProfileControllerIT.java
+++ src/test/java/bg/deliev/quasarapp/web/controller/UserProfileControllerIT.java
@@ -5,11 +5,11 @@ import bg.deliev.quasarapp.model.enums.UserRoleEnum;
 import bg.deliev.quasarapp.service.interfaces.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 import java.util.Objects;
