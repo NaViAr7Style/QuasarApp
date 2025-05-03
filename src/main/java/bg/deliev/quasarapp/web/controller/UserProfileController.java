f08f02064bfe535cc94e6eff7652e8318f19d639
NaViAr7Style <ar7styleblizz@gmail.com>
2025-05-04 01:34:09 +0300

    Added failsafe checks and refactored the controller to invalidate the session and redirect to the login page in case of a null user.

--- src/main/java/bg/deliev/quasarapp/web/controller/UserProfileController.java
+++ src/main/java/bg/deliev/quasarapp/web/controller/UserProfileController.java
@@ -2,6 +2,8 @@ package bg.deliev.quasarapp.web.controller;
 
 import bg.deliev.quasarapp.model.dto.UserDetailsDTO;
 import bg.deliev.quasarapp.service.interfaces.UserService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
@@ -18,12 +20,15 @@ public class UserProfileController {
     }
 
     @GetMapping("/user/profile")
-    public ModelAndView userProfile(@AuthenticationPrincipal UserDetails user) {
+    public ModelAndView userProfile(@AuthenticationPrincipal UserDetails user,
+                                    HttpServletRequest request) throws ServletException {
 
-        UserDetailsDTO currentUser = null;
+        UserDetailsDTO currentUser = userService.findByUsername(user.getUsername());
 
-        if (user.getUsername() != null) {
-            currentUser = userService.findByUsername(user.getUsername());
+        if (currentUser == null) {
+            request.logout();
+            request.getSession().invalidate();
+            return new ModelAndView("redirect:/login");
         }
 
         ModelAndView modelAndView = new ModelAndView("user-profile");
